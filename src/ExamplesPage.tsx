76ebbc3098cfc43b2667d1311c65013dd2c8106a
TWSiO <TWSiO@protonmail.com>
2022-01-23 21:50:42 -0800

    Adding an example page component.

---
+++ src/ExamplesPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function ExamplesPage() {
+    return (<main className="container">
+        <h1>Examples</h1>
+        <p>Some examples here</p>
+    </main>);
+}
