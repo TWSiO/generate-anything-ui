492cde7257e67a0af9860da20670e45ff827c00a
TWSiO <TWSiO@protonmail.com>
2022-04-15 19:40:05 -0700

    Adding and rearranging some header links.

--- src/Header.tsx
+++ src/Header.tsx
@@ -11,9 +11,11 @@ export default function Header() {
             <Container>
                 <LinkContainer to="/"><Navbar.Brand>Generate Anything</Navbar.Brand></LinkContainer>
                 <Nav>
+                    <LinkContainer to="/generator/create"><Nav.Link>Create generator</Nav.Link></LinkContainer>
                     <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
                     <LinkContainer to="/examples"><Nav.Link>Examples</Nav.Link></LinkContainer>
-                    <LinkContainer to="/generator/create"><Nav.Link>Create generator</Nav.Link></LinkContainer>
+                    <Nav.Link href="https://thiswebsiteis.online">Creator</Nav.Link>
+                    <Nav.Link href="https://github.com/TWSiO/generate-anything-ui">Github</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
